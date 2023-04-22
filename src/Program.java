877f26801a8ff0b73f2f658e60a0eaadf461d80d
Oleg G <oleggavrilenko1992@gmail.com>
2023-04-22 20:35:36 +0300

    task 1 numberReverse

--- src/Program.java
+++ src/Program.java
@@ -1,14 +1,25 @@
 import java.util.Scanner;
 
 public class Program {
-
     public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input a number: ");
-        int num = in.nextInt();
-
-        System.out.printf("Your number: %d \n", num);
-        in.close();
+        numberReverse();
     }
+
+    /**
+     * Пользователь задает целое трехзначное число (например, 478).
+     * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
+     */
+    private static void numberReverse() {
+        System.out.print("Task 1\nThree-digit number: ");
+        Scanner in = new Scanner(System.in);
+
+        int num = in.nextInt();
+        int hundreds = num / 100 % 100;
+        int tens = num % 100 / 10;
+        int units = num % 10;
+        int reverseNumber = units * 100 + tens * 10 + hundreds;
+
+        System.out.println("Inverted number: " + reverseNumber);
+    }
+
 }
