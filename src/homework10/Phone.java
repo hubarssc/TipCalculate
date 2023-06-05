d2d1b7f0b3845794406d3731eb0b263f87cbb310
Oleg G <oleggavrilenko1992@gmail.com>
2023-06-05 22:12:03 +0300

    OptionalAndStreamAPI

---
+++ src/homework10/Phone.java
@@ -0,0 +1,68 @@
+package homework10;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Phone {
+    public static void main(String[] args) {
+        List<Phone> phoneList = Arrays.asList(
+                new Phone("iPhone X", "2019", 600),
+                new Phone("Pixel 2", "2000", 500),
+                new Phone("iPhone 8", "2015", 450),
+                new Phone("Nokia 9", "2014", 150),
+                new Phone("Galaxy S9", "2018", 300)
+        );
+
+        sortByType(phoneList, "date"); // Сортировка по дате
+        System.out.println("--------");
+        sortByType(phoneList, "price"); // Сортировка по цене
+    }
+
+    private final String name;
+    private final String date;
+    private final int price;
+
+    public Phone(String name, String date, int price) {
+        this.name = name;
+        this.date = date;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public static void sortByType(List<Phone> phones, String sortType) {
+        Comparator<Phone> comparator;
+
+        if (sortType.equals("date")) {
+            comparator = Comparator.comparing(Phone::getDate);
+            System.out.println("Сортировка по дате:");
+        } else if (sortType.equals("price")) {
+            comparator = Comparator.comparingInt(Phone::getPrice);
+            System.out.println("Сортировка по цене:");
+        } else {
+            System.out.println("Неподдерживаемый тип сортировки.");
+            return;
+        }
+
+        phones.stream()
+                .sorted(comparator)
+                .forEach(p -> System.out.printf("%s (%s) %d\n", p.getName(), p.getDate(), p.getPrice()));
+    }
+}
+
+
+
+
+
+
